b2b7a66972fcf3a63ccf63d0ad2670a04f390c07
Sindt <czindt@gmail.com>
2015-11-04 12:26:31 +0100

    Login virker på openshift, JsonConverter tilføjet

---
+++ AngSeedServer/src/java/facades/JSONConvert.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entity.User;
+import java.util.List;
+
+/**
+ *
+ * @author Sindt
+ */
+public class JSONConvert {
+
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create();
+
+    public static User getUserFromJson(String js) {
+        return gson.fromJson(js, User.class);
+    }
+
+    public static String getJSONFromUser(User u) {
+        return gson.toJson(u);
+    }
+
+    public static String getJSONFromUsers(List<User> users) {
+        return gson.toJson(users);
+    }
+
+}
