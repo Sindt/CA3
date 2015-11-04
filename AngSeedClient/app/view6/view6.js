cf4d95ed8784fc509cacbca06e9e503ebc1cbda4
Sindt <czindt@gmail.com>
2015-11-04 17:49:26 +0100

    login fixet

--- AngSeedClient/app/view6/view6.js
+++ AngSeedClient/app/view6/view6.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('myApp.view6', ['ngRoute'])
+        .config(function ($httpProvider) {
+            $httpProvider.interceptors.push('authInterceptor');
+        })
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view6', {
-    templateUrl: 'view6/view6.html'
-  });
-}]);
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view6', {
+                    templateUrl: 'view6/view6.html'
+                });
+            }]);
