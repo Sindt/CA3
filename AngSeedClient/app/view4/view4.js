1807ecac580a2ecee63c0fb2dd4da8cb44168cbc
Sindt <czindt@gmail.com>
2015-11-05 19:42:53 +0100

    Currency Angular Table added in view4

--- AngSeedClient/app/view4/view4.js
+++ AngSeedClient/app/view4/view4.js
@@ -1,9 +1,21 @@
 'use strict';
-
 angular.module('myApp.view4', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view4', {
-    templateUrl: 'view4/view4.html'
-  });
-}]);
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view4', {
+                    templateUrl: 'view4/view4.html',
+                    controller: "View4Ctrl"
+                });
+            }])
+
+        .controller("View4Ctrl", ["$http", function ($http) {
+                var self = this;
+                $http({
+                    method: "GET",
+                    url: "api/currency/dailyrates"
+                }).then(function succesCallback(response) {
+                    self.data = response.data;
+                }).then(function errorCallback(response) {
+                    self.error = response.status;
+                })
+            }]);
