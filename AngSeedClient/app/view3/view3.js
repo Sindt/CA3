c19da631f18b9d852e42eb683d38f35713e534af
Sindt <czindt@gmail.com>
2015-11-05 10:40:30 +0100

    Accordian done, i view3

--- AngSeedClient/app/view3/view3.js
+++ AngSeedClient/app/view3/view3.js
@@ -1,15 +1,34 @@
 'use strict';
-
 angular.module('myApp.view3', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view3', {
-    templateUrl: 'view3/view3.html',
-    controller: 'View3Ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view3', {
+                    templateUrl: 'view3/view3.html',
+                    controller: 'View3Ctrl'
+                });
+            }])
 
-.controller('View3Ctrl', function($http,$scope) {
+        .controller('View3Ctrl', ["$http", function ($http) {
+                var self = this;
+                var oneAtATime = true;
+                $http({
+                    method: 'GET',
+                    url: 'http://cvrapi.dk/api?vat=3167%208021&country=dk'
+                }).then(function successCallback(response) {
+                    self.cvr = response.data;
+                }, function errorCallback(response) {
+                    self.error = response.status + " Noooo! " + response.statusText + "  upps";
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
 
- 
-});
+                self.items = ['Item 1', 'Item 2', 'Item 3'];
+                self.addItem = function () {
+                    var newItemNo = self.items.length + 1;
+                    self.items.push('Item ' + newItemNo);
+                };
+                self.status = {
+                    isFirstOpen: true,
+                    isFirstDisabled: false
+                };
+            }]);
