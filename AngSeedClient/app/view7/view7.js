994a5bec2f69900d6db2d3f01678b2badb08fea2
Sindt <czindt@gmail.com>
2015-11-06 09:55:47 +0100

    View7 added

--- AngSeedClient/app/view7/view7.js
+++ AngSeedClient/app/view7/view7.js
@@ -2,8 +2,36 @@
 
 angular.module('myApp.view7', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view7', {
-    templateUrl: 'view7/view7.html'
-  });
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view7', {
+                    templateUrl: 'view7/view7.html'
+                });
+            }])
+        
+        .controller("View7Controller", ['$scope', '$http', function($scope, $http) {
+	
+        $scope.users = [];
+	
+	$scope.addUserAsJSON = function(){		
+		$scope.users.push({ 'username':$scope.newuser.username, 'password': $scope.newuser.password});
+		// Writing it to the server
+		//		
+		var dataObj = {
+				username : $scope.newuser.username,
+				password : $scope.newuser.password,
+		};	
+		var res = $http.post('/api/user/registration', dataObj);
+		res.success(function(data, status, headers, config) {
+			$scope.message = data;
+		});
+		res.error(function(data, status, headers, config) {
+			alert( "failure message: " + JSON.stringify({data: data}));
+		});		
+		// Making the fields empty
+		//
+		$scope.newuser.username='';
+		$scope.newuser.password='';
+	};
 }]);
+
+         
